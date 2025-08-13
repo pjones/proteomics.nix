65e233ab6f2003e0de55cdb35c2f0a67e2aee911
Peter J. Jones <peter@jonesbunch.com>
2025-08-13 13:54:23 +0200

    autowrap: Add metadata and tweaks for version 0.23 (future)

--- pkgs/pyautowrap.nix
+++ pkgs/pyautowrap.nix
@@ -3,21 +3,30 @@
 , fetchPypi
 }:
 
-python3Packages.buildPythonPackage rec {
-  pname = "autowrap";
+let
   version = "0.22.11";
-  pyproject = true;
+  hash = "sha256-tGy+HysmV2mDzYWQaBpJ626M5vKCp69q/s5RsjruYrc=";
+in
+python3Packages.buildPythonPackage rec {
+  inherit version;
+  pname = "autowrap";
+  format = "pyproject";
+  src = fetchPypi { inherit pname version hash; };
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-tGy+HysmV2mDzYWQaBpJ626M5vKCp69q/s5RsjruYrc=";
-  };
-
-  build-system = with python3Packages;[
+  build-system = with python3Packages; [
     setuptools
   ];
 
   dependencies = with python3Packages; [
     cython
   ];
+
+  meta = {
+    description = "Generates Python Extension modules from Cythons PXD files";
+    mainProgram = "autowrap";
+    homepage = "https://github.com/OpenMS/autowrap/tree/master";
+    license = lib.licenses.bsd3;
+    maintainers = with lib.maintainers; [ pjones ];
+    platforms = lib.platforms.all;
+  };
 }
