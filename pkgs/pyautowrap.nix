0177e0f96e7100194493fc6fc169cb0f5f2c1bd2
Peter J. Jones <peter@jonesbunch.com>
2025-10-01 13:48:24 +0200

    OpenMS: Prepare for version 3.5

--- pkgs/pyautowrap.nix
+++ pkgs/pyautowrap.nix
@@ -1,11 +1,12 @@
-{ lib
-, python3Packages
-, fetchPypi
+{
+  lib,
+  python3Packages,
+  fetchPypi,
 }:
 
 let
-  version = "0.22.11";
-  hash = "sha256-tGy+HysmV2mDzYWQaBpJ626M5vKCp69q/s5RsjruYrc=";
+  version = "0.24.0";
+  hash = "sha256-hzU6Lgsj3jDbzJOjOfLlIUnckYzag9j3FSVe9TfQ/ZM=";
 in
 python3Packages.buildPythonPackage rec {
   inherit version;
@@ -18,13 +19,13 @@ python3Packages.buildPythonPackage rec {
   ];
 
   dependencies = with python3Packages; [
-    cython_openms
+    cython
   ];
 
   meta = {
     description = "Generates Python Extension modules from Cythons PXD files";
     mainProgram = "autowrap";
-    homepage = "https://github.com/OpenMS/autowrap/tree/master";
+    homepage = "https://github.com/OpenMS/autowrap";
     license = lib.licenses.bsd3;
     maintainers = with lib.maintainers; [ pjones ];
     platforms = lib.platforms.all;
